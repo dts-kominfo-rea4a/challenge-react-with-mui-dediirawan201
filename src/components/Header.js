dc2421d03edae5544445ea09bdbe69d9b875b123
dediirawan201 <dediirwansyah1925@gmail.com>
2022-09-26 10:30:49 +0700

    commit kedua tugas IV membuat contact

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,28 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
-import React from 'react';
-import './Header.css';
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <div className='containerHeader'>
-      <h1>Call a Friend</h1>
-      <div className='wrapperHeader'>
-      <div className='lineLeft' ></div>
-      <p className='title'>Your friendly contact app</p>
-      <div className='lineRight'></div>
-      </div>
-    </div>
+    <>
+      <Typography variant="h3" sx={{ mt: 2, mb: 3 }} component={"h3"}>
+        Call a Friend
+      </Typography>
+      <Box sx={{ borderBottom: 1, lineHeight: 0 }}>
+        <Typography
+          component={"span"}
+          variant=""
+          sx={{ bgcolor: "#F0F8FF", px: 1, py: 0 }}
+        >
+          Your friendly contact app
+        </Typography>
+      </Box>
+    </>
   );
 };
 
